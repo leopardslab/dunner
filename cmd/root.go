636c5eda5023f04a1960d415596c446be0a71c01
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2018-12-14 10:18:32 +0530

    Add root and version commands

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "dunner",
+	Short: "Dunner is a Docker based task runner",
+	Long:  `You can define a set of commands and on what Docker images these commands should run as steps. A task has many steps. Then you can run these tasks with 'dunner do nameoftask'`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+
+		fmt.Println("Dunner running!")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
