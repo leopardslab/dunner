e3d3404e673a9d71c424c01c0f2e0558f464b564
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2018-12-14 10:21:13 +0530

    Add Docker client creation check to root command
    Try to create a Docker client to make sure it can connect to Docker

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"github.com/spf13/cobra"
+	"docker.io/go-docker"
 )
 
 var rootCmd = &cobra.Command{
@@ -12,6 +13,10 @@ var rootCmd = &cobra.Command{
 	Long:  `You can define a set of commands and on what Docker images these commands should run as steps. A task has many steps. Then you can run these tasks with 'dunner do nameoftask'`,
 	Run: func(cmd *cobra.Command, args []string) {
 
+		_, err := docker.NewEnvClient()
+		if err != nil {
+			panic(err)
+		}
 
 		fmt.Println("Dunner running!")
 	},
