1e94ff4c133d7198f1b6b624493d4f92db5b30df
Ayush Jain <ayushjain20n@gmail.com>
2018-12-31 16:10:07 +0530

    [#18] Add unit test files

---
+++ pkg/config/config_test.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestGetConfigs(t *testing.T) {
+	var tmpFilename = ".testdunner.yaml"
+
+	var content = []byte(`test:
+    - image: node
+      command: ["node", "--version"]`)
+
+	tmpFile, err := ioutil.TempFile("", tmpFilename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.Write(content); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := tmpFile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	pout, err := GetConfigs(tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var task = Task{
+		Name:    "",
+		Image:   "node",
+		Command: []string{"node", "--version"},
+	}
+	var tasks = make(map[string][]Task)
+	tasks["test"] = []Task{task}
+	var expected = Configs{
+		Tasks: tasks,
+	}
+
+	if !reflect.DeepEqual(expected, *pout) {
+		t.Fatalf("Output not equal to expected")
+	}
+
+}
