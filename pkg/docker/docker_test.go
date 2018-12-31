9038bbafe6b2a0867d308d3a04b15d19d8ba1ee0
Ayush Jain <ayushjain20n@gmail.com>
2018-12-31 16:10:07 +0530

    [#18] Add unit test files

---
+++ pkg/docker/docker_test.go
@@ -0,0 +1,39 @@
+package docker
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestStep_Do(t *testing.T) {
+
+	var testNodeVersion= "10.15.0"
+
+	step := &Step{
+		Task:    "test",
+		Name:    "node",
+		Image:   "node:" + testNodeVersion,
+		Command: []string{"node", "--version"},
+		Env:     nil,
+		WorkDir: "test",
+		Volumes: nil,
+	}
+
+	pout, err := step.Do()
+	if err != nil {
+		t.Error(err)
+	}
+	buffer := new(bytes.Buffer)
+	_, err = buffer.ReadFrom(*pout)
+	if err != nil {
+		t.Error(err)
+	}
+
+	strOut := buffer.String()
+	var result = strings.Trim(strings.Split(strOut, "v")[1], "\n")
+	if result != testNodeVersion {
+		t.Fatalf("Detected version of node container: '%s'; Expected output: '%s'", result, testNodeVersion)
+	}
+
+}
