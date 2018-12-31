b6635caf1ed884556da90e26909fbfa116c9b200
Ayush Jain <ayushjain20n@gmail.com>
2018-12-31 22:09:43 +0530

    Replace 'panic' with 'log.Fatal' in 'dunner' package for handling errors

--- pkg/dunner/dunner.go
+++ pkg/dunner/dunner.go
@@ -4,6 +4,7 @@ import (
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/leopardslab/Dunner/pkg/config"
 	"github.com/leopardslab/Dunner/pkg/docker"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"os"
 )
@@ -15,7 +16,7 @@ func Do(_ *cobra.Command, args []string) {
 
 	configs, err := config.GetConfigs(dunnerFile)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	for _, stepDefinition := range configs.Tasks[args[0]] {
@@ -27,12 +28,12 @@ func Do(_ *cobra.Command, args []string) {
 		}
 		pout, err := step.Do()
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		_, err = stdcopy.StdCopy(os.Stdout, os.Stderr, *pout)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}
 
