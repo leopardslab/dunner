b77fe53148924c34554fe04922188f48e4c84c46
Ayush Jain <ayushjain20n@gmail.com>
2019-01-02 19:19:02 +0530

    Customize time format in logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,8 +8,9 @@ import(
 var Log = logrus.New()
 
 func init() {
-	Log.Formatter = new(logrus.TextFormatter)                     // default
-	Log.Formatter.(*logrus.TextFormatter).DisableTimestamp = true // remove timestamp from test output
+	Log.Formatter = new(logrus.TextFormatter)					// default
+	Log.Formatter.(*logrus.TextFormatter).FullTimestamp = true	// Enable timestamp
+	Log.Formatter.(*logrus.TextFormatter).TimestampFormat = "2006-01-02 15:04:05"	// Customize timestamp format
 	Log.Level = logrus.TraceLevel
 	Log.Out = os.Stdout
 }
