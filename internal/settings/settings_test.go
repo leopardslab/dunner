57809489202a701edfaef816125b5b06def66c60
Ayush Jain <ayushjain20n@gmail.com>
2019-08-08 02:34:38 +0530

    Add unit test for default settings initialisation

---
+++ internal/settings/settings_test.go
@@ -0,0 +1,26 @@
+package settings
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func TestInit(t *testing.T) {
+	Init()
+	defaultSettings := map[string]interface{}{
+		"dunnertaskfile":   ".dunner.yaml",
+		"dotenvfile":       ".env",
+		"globallogfile":    "/var/log/dunner/logs/",
+		"workingdirectory": "./",
+		"async":            false,
+		"verbose":          false,
+		"dry-run":          false,
+		"dockerapiversion": "1.39",
+	}
+
+	if !reflect.DeepEqual(viper.AllSettings(), defaultSettings) {
+		t.Fatal("Default not equal to as expected")
+	}
+}
